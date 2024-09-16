87d913fe8349745aedb8282f8560a34169439354
nikishkaa <nikita.goutor@gmail.com>
2024-09-16 16:19:25 +0300

    Update pom.xml Update User.java

--- src/main/java/org/example/finaljavaeeproject/entity/User.java
+++ src/main/java/org/example/finaljavaeeproject/entity/User.java
@@ -1,4 +1,50 @@
 package org.example.finaljavaeeproject.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "User")
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "name", length = 128, nullable = false)
+    private String name;
+
+    @Column(name = "email", length = 128, nullable = false)
+    private String email;
+
+    @Column(name = "password", length = 128, nullable = false)
+    private String password;
+
+    @Column(name = "phone_number", length = 20, nullable = true)
+    private String phoneNumber;
+
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+
+    @Column(name = "is_active", length = 1, nullable = false)
+    private String isActive;
+
+    @Column(name = "created_ts", nullable = false)
+    @CreationTimestamp
+    private Timestamp createdTs;
+
+    @Column(name = "updated_ts")
+    private Timestamp updatedTs;
 }
